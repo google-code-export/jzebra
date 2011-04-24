964a25917814e3b2b756fcceb4927c33af275326
tres.finocchiaro@gmail.com <tres.finocchiaro@gmail.com@46e91e78-2a53-0410-bce8-1923352d0024>
2011-04-24 05:02:32 +0000

    ZPLII image support (additional fixes)

---
+++ jZebra/src/jzebra/LanguageType.java
@@ -0,0 +1,31 @@
+package jzebra;
+
+/**
+ * Enum for print languages, such as ZPL, EPL, etc.
+ * @author tfino
+ */
+public enum LanguageType {
+
+    ZPLII, ZPL, EPL2, EPL, CPCL, ESCP, UNKNOWN;
+
+    LanguageType() {
+    }
+
+    public static LanguageType getType(String s) {
+        for (LanguageType lt : LanguageType.values()) {
+            if (s.equalsIgnoreCase(lt.name())) {
+                return lt;
+            }
+        }
+        if (s.equalsIgnoreCase("ZEBRA")) {
+            return ZPLII;
+        } else if (s.equalsIgnoreCase("ZPL2")) {
+            return ZPLII;
+        } else if (s.equalsIgnoreCase("EPLII")) {
+            return EPL2;
+        } else if (s.equalsIgnoreCase("EPSON")) {
+            return ESCP;
+        }
+        return UNKNOWN;
+    }
+}
